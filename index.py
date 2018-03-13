9531f3917e17b5c9306fd0a49109e49f04503390
Songkc <weijia_sysu@163.com>
2018-03-13 14:23:30 +0800

    Modify file structrue, add 404 page

--- index.py
+++ index.py
@@ -128,5 +128,9 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     manager.run()
