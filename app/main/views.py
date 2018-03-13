c44dc3c346bcf7b89ca8921bd22e3998c6528b3c
psy054duck <wangchlin0@163.com>
2018-03-13 16:59:03 +0800

    remove useless codes

--- app/main/views.py
+++ app/main/views.py
@@ -4,10 +4,8 @@ import random
 from . import main
 from flask import Flask, render_template, redirect, url_for, flash, session
 from flask import request
-from flask_script import Manager
-from flask_bootstrap import Bootstrap
-from flask_login import UserMixin, LoginManager, login_user, login_required
-from flask_login import current_user, logout_user
+from flask_login import UserMixin, login_required
+from flask_login import current_user, logout_user, login_user
 from werkzeug.utils import secure_filename
 
 from .forms import LoginForm, EditForm
